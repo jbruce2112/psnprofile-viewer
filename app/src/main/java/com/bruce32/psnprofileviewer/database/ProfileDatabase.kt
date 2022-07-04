8201fba7fb580dc1aed2e014061967af2be9e0b5
John Bruce <jbruce2112@gmail.com>
2022-07-04 14:25:59 -0700

    v1

--- app/src/main/java/com/bruce32/psnprofileviewer/database/ProfileDatabase.kt
+++ app/src/main/java/com/bruce32/psnprofileviewer/database/ProfileDatabase.kt
@@ -1,12 +1,10 @@
 package com.bruce32.psnprofileviewer.database
 
-import android.content.Context
 import androidx.room.Dao
 import androidx.room.Database
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.bruce32.psnprofileviewer.model.Game
@@ -16,7 +14,7 @@ import kotlinx.coroutines.flow.Flow
 
 @Database(
     entities = [Profile::class, Game::class, Trophy::class],
-    version = 2,
+    version = 1,
     exportSchema = false
 )
 @TypeConverters(ProfileTypeConverters::class)
